2388e5b6f4117b051698538547fd3e8958316edf
Cameron <cpolly@pgh.yearup.org>
2024-10-10 17:03:20 -0400

    Updated toString Override

--- OnlineStore/src/main/java/com/pluralsight/Product.java
+++ OnlineStore/src/main/java/com/pluralsight/Product.java
@@ -4,6 +4,6 @@ public record Product(String id, String name, double price) {
 
     @Override
     public String toString() {
-        return "id: " + id + ", name: '" + name + "', price: " + price;
+        return "ID: " + id + ", Name: '" + name + "', Price: $" + price;
     }
 }
