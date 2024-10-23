d59f5993575d7db4c23960d39f117c3638b33ffc
Cameron <cpolly@pgh.yearup.org>
2024-10-23 19:27:42 -0400

    Modified Main Loop Input To Use String Rather Than int

--- OnlineStore/src/main/java/com/pluralsight/main/Store.java
+++ OnlineStore/src/main/java/com/pluralsight/main/Store.java
@@ -15,20 +15,20 @@ public class Store {
 
     public static void main(String[] args) {
         INVENTORY.loadInventoryProducts(PRODUCTS_PATH);
+        String choice = "";
 
-        int choice = -1;
 
         // Display menu and get user choice until they choose to exit
-        while (choice != 3) {
+        while (!choice.equals("3")) {
+
             System.out.println("\nWelcome to the Online Store!\n\t1. Show Products\n\t2. Show Cart\n\t3. Exit");
-            choice = SCANNER.nextInt();
-            SCANNER.nextLine();
+            choice = SCANNER.nextLine();
 
             // Call the appropriate method based on user choice
             switch (choice) {
-                case 1 -> InputManager.productMenuDisplay(INVENTORY,SCANNER,CART);
-                case 2 -> InputManager.handleCartInput(SCANNER,CART);
-                case 3 -> System.out.println("\nThank you for shopping with us!");
+                case "1" -> InputManager.productMenuDisplay(INVENTORY,SCANNER,CART);
+                case "2" -> InputManager.handleCartInput(SCANNER,CART);
+                case "3" -> System.out.println("\nThank you for shopping with us!");
                 default -> System.out.println("Invalid choice!");
             }
         }
